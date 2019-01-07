f6dd303de56a13c838512429b8c16eba4f84ecc5
Shamit <github@shamit.in>
2019-01-07 23:27:34 +0530

    Changed loss function

--- human_protein/keras_model/model_def.py
+++ human_protein/keras_model/model_def.py
@@ -3,15 +3,24 @@ from keras.layers import Conv2D, Activation, Dropout
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
+from keras import backend as K
+import tensorflow as tf
 
 from config.paths import data_dir
 from config.data import num_classes , image_dimension
 
 def getModelFile():
     model_dir = data_dir + '/model'
-    model_version = 3
+    model_version = 4
     model_path = "{0}/model_v{1}.h5".format(model_dir,model_version)
     return model_path
+
+def focal_loss(gamma=2., alpha=.25):
+    def focal_loss_fixed(y_true, y_pred):
+        pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
+        pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
+        return -K.sum(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1))-K.sum((1-alpha) * K.pow( pt_0, gamma) * K.log(1. - pt_0))
+    return focal_loss_fixed
 
 def getModel():
     model = Sequential()
@@ -35,9 +44,9 @@ def getModel():
     model.add(Dropout(0.5))
     model.add(Dense(num_classes, activation='sigmoid'))
 
-    model.compile(loss='binary_crossentropy',
+    model.compile(loss=[focal_loss(alpha=.25, gamma=2)],
                   optimizer='adam',
-                  metrics=['accuracy','categorical_accuracy'])
+                  metrics=['accuracy'])
 
     model.summary()
     return  model
