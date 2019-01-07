82441b536b581f8e03ff72cf6fb7c3397f0cbc17
Shamit <github@shamit.in>
2019-01-07 23:38:28 +0530

    Fix NaN for focal loss

--- human_protein/keras_model/model_def.py
+++ human_protein/keras_model/model_def.py
@@ -15,6 +15,17 @@ def getModelFile():
     model_path = "{0}/model_v{1}.h5".format(model_dir,model_version)
     return model_path
 
+def focal_loss3(gamma=2., alpha=.25):
+    def focal_loss_fixed(y_true, y_pred):
+        pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
+        pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
+
+        pt_1 = K.clip(pt_1, 1e-3, .999)
+        pt_0 = K.clip(pt_0, 1e-3, .999)
+
+        return -K.sum(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1))-K.sum((1-alpha) * K.pow( pt_0, gamma) * K.log(1. - pt_0))
+    return focal_loss_fixed
+
 def focal_loss2(gamma=2, alpha=0.75):
     def focal_loss_fixed(y_true, y_pred):#with tensorflow
         eps = 1e-12
@@ -53,7 +64,7 @@ def getModel():
     model.add(Dropout(0.5))
     model.add(Dense(num_classes, activation='sigmoid'))
 
-    model.compile(loss=[focal_loss2(alpha=.25, gamma=2)],
+    model.compile(loss=[focal_loss3(alpha=.25, gamma=2)],
                   optimizer='adam',
                   metrics=['accuracy'])
 
