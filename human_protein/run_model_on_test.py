d786baeca4a119e200e708d0985bb143331a9a6a
Shamit <github@shamit.in>
2019-01-08 15:19:21 +0530

    Run model on test with pdated loss and metric functions

--- human_protein/run_model_on_test.py
+++ human_protein/run_model_on_test.py
@@ -1,6 +1,6 @@
-from keras_model.model_def import getModel, getModelFile
+from keras_model.model_def import focal_loss , f1, precision, recall
+from config.paths import data_dir, test_results_pickle
 from dataset.loadData import getData, getCSVDataFrame
-from config.paths import test_results_pickle
 from logging import info
 import keras
 import pickle
@@ -15,15 +15,17 @@ info('Getting model definition')
 
 
 
-model_path = getModelFile()
+model_path =  data_dir + 'model/' + 'model_v8.h5.0103-18.27val_f1-0.2443.h5'
 
 info("Loading model from :: {0}".format(model_path))
 
-model = keras.models.load_model(model_path)
+focal_loss_fixed=focal_loss(alpha=.25, gamma=2)
+model = keras.models.load_model(model_path, custom_objects={'focal_loss_fixed': focal_loss_fixed ,
+                                                            'f1':f1, 'precision':precision, 'recall':recall})
 
 test_x = test_data['test_x']
 info('Predicting classes')
-results=model.predict(test_x, batch_size=2)
+results=model.predict(test_x, batch_size=2, verbose=1)
 
 pickle_data = {'test_results': results , 'image_ids' : image_ids}
 info("Saving data to pickle :: {0} ".format(test_results_pickle))
