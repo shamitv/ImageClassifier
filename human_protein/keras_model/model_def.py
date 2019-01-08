3e52250520a8038653b7c2c792055df2520a48eb
Shamit <github@shamit.in>
2019-01-08 10:32:57 +0530

    Changed accuracy metric

--- human_protein/keras_model/model_def.py
+++ human_protein/keras_model/model_def.py
@@ -11,11 +11,11 @@ from config.data import num_classes , image_dimension
 
 def getModelFile():
     model_dir = data_dir + '/model'
-    model_version = 5
+    model_version = 6
     model_path = "{0}/model_v{1}.h5".format(model_dir,model_version)
     return model_path
 
-def focal_loss3(gamma=2., alpha=.25):
+def focal_loss(gamma=2., alpha=.25):
     def focal_loss_fixed(y_true, y_pred):
         pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
         pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
@@ -26,21 +26,21 @@ def focal_loss3(gamma=2., alpha=.25):
         return -K.sum(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1))-K.sum((1-alpha) * K.pow( pt_0, gamma) * K.log(1. - pt_0))
     return focal_loss_fixed
 
-def focal_loss2(gamma=2, alpha=0.75):
-    def focal_loss_fixed(y_true, y_pred):#with tensorflow
-        eps = 1e-12
-        y_pred=K.clip(y_pred,eps,1.-eps)#improve the stability of the focal loss and see issues 1 for more information
-        pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
-        pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
-        return -K.sum(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1))-K.sum((1-alpha) * K.pow( pt_0, gamma) * K.log(1. - pt_0))
-    return focal_loss_fixed
+def recall(y_true, y_pred):
+    #Recall metric.
 
-def focal_loss(gamma=2., alpha=.25):
-    def focal_loss_fixed(y_true, y_pred):
-        pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
-        pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
-        return -K.sum(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1))-K.sum((1-alpha) * K.pow( pt_0, gamma) * K.log(1. - pt_0))
-    return focal_loss_fixed
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)),axis=0)
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)),axis=0)
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
+def precision(y_true, y_pred):
+    #Precision metric.
+
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)),axis=0)
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)),axis=1)
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
 
 def getModel():
     model = Sequential()
@@ -65,9 +65,9 @@ def getModel():
     model.add(Dropout(0.5))
     model.add(Dense(num_classes, activation='sigmoid'))
 
-    model.compile(loss=[focal_loss3(alpha=.25, gamma=2)],
+    model.compile(loss=[focal_loss(alpha=.25, gamma=2)],
                   optimizer='adam',
-                  metrics=['accuracy'])
+                  metrics=[precision,recall])
 
     model.summary()
     return  model
