537dd4c8534b96270b6c22868bd9c079f6455041
Shamit <github@shamit.in>
2019-01-09 19:35:21 +0530

    Calc F1 score on each Epoch

--- human_protein/train_single_class_models.py
+++ human_protein/train_single_class_models.py
@@ -6,11 +6,39 @@ from keras.preprocessing.image import ImageDataGenerator
 from config.paths import processed_image_dir_train
 from config.data import image_dimension
 import logging
+import numpy as np
+from keras.callbacks import Callback
+from sklearn.metrics import confusion_matrix, f1_score, precision_score, recall_score
+
 logging.basicConfig(level=logging.INFO,  format='%(asctime)s - %(message)s')
 
 batch_size=32
 
+
+class F1_Score_Metrics(Callback):
+    def on_train_begin(self, logs={}):
+        self.val_f1s = []
+        self.val_recalls = []
+        self.val_precisions = []
+
+
+def on_epoch_end(self, epoch, logs={}):
+    val_predict = (np.asarray(self.model.predict(self.model.validation_data[0]))).round()
+    val_targ = self.model.validation_data[1]
+    _val_f1 = f1_score(val_targ, val_predict)
+    _val_recall = recall_score(val_targ, val_predict)
+    _val_precision = precision_score(val_targ, val_predict)
+    self.val_f1s.append(_val_f1)
+    self.val_recalls.append(_val_recall)
+    self.val_precisions.append(_val_precision)
+    print (" — val_f1: {0} — val_precision: {1} — val_recall {2}".format(_val_f1, _val_precision, _val_recall))
+    return
+
+
+
+
 def trainForLabel(label , df, class_weights):
+    f1_metrics = F1_Score_Metrics()
     y_column_name='label_'+str(label)
     num_samples=df.shape[0]
     validation_split=0.2
@@ -38,7 +66,7 @@ def trainForLabel(label , df, class_weights):
 
     model.fit_generator(generator=train_generator,
                         steps_per_epoch=num_train_steps, epochs=20,verbose=1,
-                        callbacks=[checkpoint],
+                        callbacks=[checkpoint,f1_metrics],
                         validation_steps=num_val_steps,
                         validation_data=validation_generator,
                         class_weight=class_weights)
