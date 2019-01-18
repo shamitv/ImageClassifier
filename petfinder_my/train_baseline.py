b14ceff17b57de6445be5340c8e12862b8df4c7d
Shamit <github@shamit.in>
2019-01-18 21:34:53 +0530

    Tweak network

--- petfinder_my/train_baseline.py
+++ petfinder_my/train_baseline.py
@@ -14,7 +14,7 @@ train_y=convertColumnToOneHot(train_y,'AdoptionSpeed',[0,1,2,3,4])
 num_classes=5
 num_columns=train_X.shape[1]
 
-checkpoint=ModelCheckpoint(getModelFile(), monitor='val_loss' )
+checkpoint=ModelCheckpoint(getModelFile(), monitor='val_loss' , verbose=1 , save_best_only=True)
 
 model=getModel(num_classes,num_columns)
 model.fit(train_X, train_y, validation_split=0.2, epochs=1600,
