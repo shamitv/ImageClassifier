c4aa5badd41552f946102f25cd65bae9e83911a1
shamit <github@shamit.in>
2019-01-18 14:04:01 +0000

    Checkpoint trained model

--- petfinder_my/train_baseline.py
+++ petfinder_my/train_baseline.py
@@ -1,6 +1,5 @@
-
-
-from keras_model.model_def import getModel
+from keras.callbacks import ModelCheckpoint
+from keras_model.model_def import getModel, getModelFile
 from logging import info
 
 from dataset.pre_process import getTrainDF , convertColumnToOneHot
@@ -8,17 +7,15 @@ from keras_model.tensorboard import getTensorboardCallback
 info('Getting data')
 
 df=getTrainDF()
-
 info('Got data')
-
-
-
 train_X = df.drop(columns=['AdoptionSpeed'])
 train_y = df[['AdoptionSpeed']]
-
 train_y=convertColumnToOneHot(train_y,'AdoptionSpeed',[0,1,2,3,4])
 num_classes=5
 num_columns=train_X.shape[1]
 
+checkpoint=ModelCheckpoint(getModelFile(), monitor='val_loss' )
+
 model=getModel(num_classes,num_columns)
-model.fit(train_X, train_y, validation_split=0.2, epochs=800, verbose=2, callbacks=[getTensorboardCallback()])
+model.fit(train_X, train_y, validation_split=0.2, epochs=800,
+          verbose=2, callbacks=[getTensorboardCallback(),checkpoint])
